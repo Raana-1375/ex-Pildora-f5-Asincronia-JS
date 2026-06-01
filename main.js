a035bae98eea032cbd192bcf41aee84104d44fe4
Raana <raanaafkari96@gmail.com>
2026-06-01 09:23:15 +0200

    add the index.html content

---
+++ main.js
@@ -0,0 +1,19 @@
+
+//synchronous/asynchronous code/callback function
+console .log("1. order placed");
+setTimeout(() => {
+    console .log("2. the pizza is ready");
+}, 3000);
+console .log("3. serve drink to another tables");
+
+//example of promise/await
+const cookPizza = () => new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("2.the pizza is ready with the promotion");
+    }, 3000);
+});
+const promotiontopromote = async () => { console.log("1. order placed");
+     const result = await cookPizza(); 
+     console.log(result); console.log("3. serve drink to another tables"); };
+setTimeout(() => { promotiontopromote();
+}, 3000);
